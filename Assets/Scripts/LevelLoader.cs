f8489f1d2448430b5694eb947db9b84d8e115eb1
Sergey Alemasov <s.alemasov@tinkoff.ru>
2019-10-30 13:43:01 +0400

    added initial scenes, sfx and scripting

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int splashScreenVisibilityTime = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupSplashScreen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetupSplashScreen()
+    {
+        if (SceneManager.GetActiveScene().name != "Splash Screen")
+        {
+            return;
+        }
+
+        StartCoroutine(LoadStartScreenWithDelay());
+    }
+
+    IEnumerator LoadStartScreenWithDelay()
+    {
+        yield return new WaitForSeconds(splashScreenVisibilityTime);
+
+        SceneManager.LoadScene("Start Screen");
+    }
+}
