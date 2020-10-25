deb206d31a0e9ba7ff90d99d18b2d044ada3b1bf
Alemasov Sergey Aleksandrovich <s.alemasov@tinkoff.ru>
2020-10-25 17:19:27 +0400

    added attacker script

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range (0f, 5f)]
+    [SerializeField]
+    private float walkSpeed = 1f;
+    // Start is called before the first frame update 
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(walkSpeed * Time.deltaTime * Vector2.left);
+    }
+}
